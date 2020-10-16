2428b128b6a3617cbb1ac5127270905f12642aeb
wb_glb <weibin9966@qq.com>
2020-10-16 16:32:26 +0800

    初始化

---
+++ src/test/java/jenkins/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkins.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
